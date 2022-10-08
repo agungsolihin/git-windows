65e7e3f2186405bbc7906b063245f5c15e2b0e99
agungsolihin <mohagungsolihin@gmail.com>
2022-10-09 06:20:45 +0700

    first code

---
+++ test.py
@@ -0,0 +1,12 @@
+for x in range(0,4):
+    print(x)
+    if x == 2:
+        print("yay,, I found two number")
+
+website = "duniailkom"
+harga = 350.25
+sukses = 4j
+
+print(type(website))
+print(type(harga))
+print(type(sukses))
